743bfb512b3362ea80ce19bd9d0b7202f5db0218
Martin Icking <martin.icking@bentley.com>
2015-08-21 10:22:46 +0200

    Update of jmeter.bat to avoid the creation of an extra command window on startup

--- tools/jmeter.bat
+++ tools/jmeter.bat
@@ -1,1 +1,5 @@
-start %ChocolateyInstall%\lib\jmeter\tools\apache-jmeter-2.13\bin\jmeter.bat %ARGS%
+@set JM_START=start
+@set JM_LAUNCH=javaw.exe
+@call %ChocolateyInstall%\lib\jmeter\tools\apache-jmeter-2.13\bin\jmeter.bat %ARGS%
+@set JM_START=
+@set JM_LAUNCH=
